7b1bed9b0d7799271b484d108facdf81c44220ad
Luciano Renovato de Albuquerque <luciano.ra@gmail.com>
2020-02-18 22:50:11 -0300

    Teste de transicao de telas.

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,10 @@ void main() => runApp(MaterialApp(
     theme:
     ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
     debugShowCheckedModeBanner: false,
-    home: SplashScreen(),
+    home: SplashScreenGreen(),
   ));
 
+/*
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -16,7 +17,7 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   void initState(){
     super.initState();
-    Timer(Duration(seconds: 5), ()=> print("Splash Done!"));
+    Timer(Duration(seconds: 5), ()=> Navigator.of(context).push(_createRoute()));
   }
 
   @override
@@ -87,4 +88,178 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
+*/
 
+Route _createRouteBlue() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => SplashScreenBlue(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+Route _createRouteGreen() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => SplashScreenGreen(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+class SplashScreenBlue extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Timer(Duration(seconds: 10), ()=> Navigator.of(context).push(_createRouteGreen()));
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.blueAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton(
+                      child: Text('Go green!'),
+                      onPressed: () {
+                          Navigator.of(context).push(_createRouteGreen());
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                          "FlutKart", style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight:
+                          FontWeight.bold)
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(),
+                      Padding(
+                        padding: EdgeInsets.only(top:20.0),
+                      ),
+                      Text(
+                          "Online Store \nFor You!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold
+                          )
+                      )
+                    ],
+                  )
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SplashScreenGreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //Timer(Duration(seconds: 5), ()=> Navigator.of(context).push(_createRouteBlue()));
+
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.greenAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton(
+                        child: Text('Go blue!'),
+                        onPressed: () {
+                          Navigator.of(context).push(_createRouteBlue());
+                        },
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                          "FlutKart", style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24.0,
+                          fontWeight:
+                          FontWeight.bold)
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(),
+                      Padding(
+                        padding: EdgeInsets.only(top:20.0),
+                      ),
+                      Text(
+                          "Online Store \nFor Us!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold
+                          )
+                      )
+                    ],
+                  )
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
