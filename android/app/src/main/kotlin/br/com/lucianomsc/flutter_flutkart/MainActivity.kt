d57eef2e2bd47ea9fb3546e85fd5b19cb585d77d
Luciano Renovato de Albuquerque <luciano.ra@gmail.com>
2020-02-18 21:16:28 -0300

    Implementacao de uma splash screen.

---
+++ android/app/src/main/kotlin/br/com/lucianomsc/flutter_flutkart/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.lucianomsc.flutter_flutkart
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
